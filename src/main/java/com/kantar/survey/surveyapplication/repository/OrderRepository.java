8e6bd8100e769965fc853fb082f1f77962e6c7c9
RajamohanNarala <85684499+RajamohanNarala@users.noreply.github.com>
2021-07-01 16:25:14 +0530

    Create OrderRepository.java
    ok

---
+++ src/main/java/com/kantar/survey/surveyapplication/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.kantar.survey.surveyapplication.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kantar.survey.surveyapplication.model.Order;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Integer>{
+
+}
