f145ba9cccac031ad70440e1a3e277070369cded
RajamohanNarala <85684499+RajamohanNarala@users.noreply.github.com>
2021-07-01 16:37:28 +0530

    Create SurveyApplicationTest.java
    ok

---
+++ src/main/test/java/com/kantar/survey/surveyapplication/SurveyApplicationTest.java
@@ -0,0 +1,13 @@
+package com.kantar.survey.surveyapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
